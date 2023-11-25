33a7fc0d3696cb979d4055b3a5e3e0e42335af2a
Esperanza <esperanzasj2012@gmail.com>
2023-11-25 19:13:30 -0400

    Add recordado in the DB and config

--- src/app/api/correo/route.ts
+++ src/app/api/correo/route.ts
@@ -1,8 +1,28 @@
 import { env } from "@/env.mjs";
+import { db } from "@/server/db";
 import { createTransport } from "nodemailer";
 
 export const dynamic = "force-dynamic"; // defaults to force-static
-export async function GET(){
+export async function GET() {
+  const tareasPorRecordar = await db.tarea.findMany({
+    where: {
+      recordado: false,
+      vencimiento: {
+        lte: new Date(),
+      },
+    },
+    select: {
+      createdBy: {
+        select: {
+          email: true,
+        },
+      },
+      id: true,
+      titulo: true,
+      descripcion: true,
+    },
+  });
+
   const transport = createTransport({
     auth: {
       user: env.USER_EMAIL,
@@ -12,21 +32,27 @@ export async function GET(){
     port: 465,
   });
 
-  const result = await transport.sendMail({
-    to: "esperanzasj2012@gmail.com",
-    from: "esperanzasj2012@gmail.com",
-    subject: "prueba",
-    text: "hola",
-    headers: {
-      "X-Entity-Ref-ID": new Date().getTime().toString(),
-    },
-  });
+  for (const recordatorio of tareasPorRecordar) {
+    const result = await transport.sendMail({
+      to: recordatorio.createdBy.email ?? undefined,
+      from: "esperanzasj2012@gmail.com",
+      subject: `Recordatorio ${recordatorio.titulo}`,
+      text: `Hola! Tu tarea ${recordatorio.titulo}
+      con la descripcion: ${recordatorio.descripcion}
+      expiro.
+      `,
+      headers: {
+        "X-Entity-Ref-ID": new Date().getTime().toString(),
+      },
+    });
 
-  const failed = result.rejected.concat(result.pending).filter(Boolean);
-  if (failed.length) {
-    throw new Error();
+    const failed = result.rejected.concat(result.pending).filter(Boolean);
+    if (failed.length) {
+      throw new Error();
+    }
   }
-  return new Response('Hola, se vencio una de tus tarea, revisa la pagina', {
+
+  return new Response("Hola, se vencio una de tus tarea, revisa la pagina", {
     status: 200,
-  })
+  });
 }
