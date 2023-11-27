5e43be0e0b55d5d31060dc1d993197dc893730fa
Esperanza <esperanzasj2012@gmail.com>
2023-11-27 09:14:56 -0400

    Add logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,8 @@ export default async function Navbar() {
   const session = await getServerAuthSession();
   return (
     <nav className="flex items-center justify-between bg-slate-900 px-40 py-3 text-white">
+      <a href="./" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="https://solucionescontables.net.ve/wp-content/uploads/2023/09/azul_rojo.png" className="h-8" alt="" /></a>
       <Link href="/">
         <h1>Administrador de Tareas Solcon Sa</h1>
       </Link>
